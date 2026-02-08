6edcafd7d2a7e159a86d9c2c1e733a0f416ff0fb
Christophe Chanteur <soundsliced@gmail.com>
2026-02-08 15:14:34 +0000

    Release version 2.0.0
    - Updated version, documentation, and configuration
    - Organized scripts and added .gitattributes/.pubignore

--- lib/s_liquid_pull_to_refresh.dart
+++ lib/s_liquid_pull_to_refresh.dart
@@ -3,4 +3,4 @@
 /// This library exports all public APIs for the s_liquid_pull_to_refresh package.
 library;
 
-export 'src/s_liquid_pull_to_refresh.dart';
+export 'package:s_packages/s_packages.dart';
--- test/s_liquid_pull_to_refresh_test.dart
+++ test/s_liquid_pull_to_refresh_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:s_liquid_pull_to_refresh/s_liquid_pull_to_refresh.dart';
 
